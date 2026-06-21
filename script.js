57e4204435361ac662a2ae3cc61596cc30b3f2c4
nancy798 <rybalchenkoa654@gmail.com>
2026-06-22 00:14:54 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function updateClock() {
+    let now = new Date();
+
+    document.getElementById("clock").innerHTML =
+        now.toLocaleTimeString();
+}
+
+setInterval(updateClock, 1000);
+updateClock();
