379fc7455024dc6bef312aabff86c8dab68d0290
nancy798 <rybalchenkoa654@gmail.com>
2026-06-22 23:50:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,69 @@
-function updateClock() {
-    let now = new Date();
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll(".open-iframe").forEach(btn => {
+        btn.addEventListener("click", function(e) {
+            e.preventDefault();
 
-    document.getElementById("clock").innerHTML =
-        now.toLocaleTimeString();
-}
+            const lesson = this.closest(".lesson");
+            const frameBox = lesson.querySelector(".frameBox");
+            const url = this.href;
 
-setInterval(updateClock, 1000);
-updateClock();
+            // якщо iframe вже є → закрити
+            if (frameBox.querySelector("iframe")) {
+                frameBox.innerHTML = "";
+                return;
+            }
+
+            // інакше відкрити
+            frameBox.innerHTML = `
+                <iframe src="${url}" allowfullscreen></iframe>
+            `;
+        });
+    });
+});
+
+
+window.carousel = () => {
+  let handleScroll, timer, cleanup;
+
+  return {
+    scrolledStart: false,
+    scrolledEnd: false,
+
+    init() {
+      handleScroll = () => {
+        if (timer) clearTimeout(timer);
+
+        timer = setTimeout(() => {
+          this.scrolledEnd =
+            Math.ceil(this.$refs.items.scrollLeft + this.$refs.items.clientWidth) >=
+            Math.ceil(this.$refs.items.scrollWidth);
+
+          this.scrolledStart = this.$refs.items.scrollLeft <= 0;
+        }, 10);
+      };
+
+      handleScroll();
+      this.$refs.items.addEventListener("scroll", handleScroll);
+      addEventListener("resize", handleScroll);
+
+      cleanup = () => {
+        this.$refs.items.removeEventListener("scroll", handleScroll);
+        removeEventListener("resize", handleScroll);
+      };
+    },
+
+    next() {
+      this.$refs.items.scrollBy({
+        left: this.$refs.items.clientWidth,
+        behavior: "smooth"
+      });
+    },
+
+    prev() {
+      this.$refs.items.scrollBy({
+        left: -this.$refs.items.clientWidth,
+        behavior: "smooth"
+      });
+    }
+  };
+};
